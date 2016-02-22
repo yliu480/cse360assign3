eed69d2a88057f457567a398c8d6c533386f45b6
yliu480 <yliu480@asu.edu>
2016-02-21 23:50:13 -0700

    initial checkin calculator

---
+++ CSE360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,78 @@
+
+package cse360assign3;
+
+/**
+ * The class can do multiple calculations. 
+ * 
+ * PIN: 512
+ * 
+ * @author Yi Liu for CSE360 Spring 2016
+ * @version Feb 21, 2016.
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * constructor for Calculator()
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * get the result of calculations
+	 * 
+	 * @return the result of calculation. 
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add a input value to result 
+	 * 
+	 * @param value the value need to be added to result
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract an input value from result 
+	 * 
+	 * @param value the value need to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply an input value to the result 
+	 * 
+	 * @param value the value need to be multiple
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide the result by an input value 
+	 * 
+	 * @param value the value will divide the result
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * get the history of calculation 
+	 * 
+	 * @return a string with expression how we get the result
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ CSE360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,47 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator test = new Calculator();
+		assertNotNull(test);
+	}
+	
+	@Test
+	public void getTotal () {
+		fail("Not build yet");
+	}
+	
+	@Test
+	public void add () {
+		fail("Not build yet");
+	}
+	
+	@Test
+	public void subtract () {
+		fail("Not build yet");
+	}
+	
+	@Test
+	public void multiply () {
+		fail("Not build yet");
+	}
+	
+	@Test
+	public void divide () {
+		fail("Not build yet");
+	}
+	
+	@Test
+	public void getHistory () {
+		fail("Not build yet");
+	}
+
+
+}
